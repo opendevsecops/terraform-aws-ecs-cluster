44b0affb4819c2e3b6170c4e9cf7783e0eace834
Petko D. Petkov <pdp@gnucitizen.org>
2018-11-08 14:32:50 +0000

    First.

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "name" {
+  description = "The name of the cluster"
+}
+
+variable "vpc_cidr_block" {
+  description = "The cluster vpc cidr block"
+}
+
+variable "subnet_cidr_block" {
+  description = "The cluster subnet cidr block"
+}
